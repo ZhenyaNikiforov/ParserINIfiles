8785670f0b87a36011f924a9c7d4e7b0cd5d9168
Zhenya Nikiforov <12A92@mail.ru>
2026-02-06 17:30:56 +0500

    Карта данных, шаблонный метод класса

--- main.cpp
+++ main.cpp
@@ -3,43 +3,31 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <vector>
+#include <map>
 #include <regex>
+#include <type_traits>
 using namespace std;
 
 class ini_parser // класс нашего парсера
 {
 public:
-  ini_parser(string fileName) // конструктор, принимает имя (и путь) к файлу
+  ini_parser(string pathAndNameFile) // конструктор, принимает имя (и путь) к файлу
   {
-    this->pathToFile = fileName; // заносим имя и путь к файлу в поле экземпляра;
-  };
+    ifstream iniFile(pathAndNameFile); // открываем ini-файл
 
-  string getValue(string adressLine) // подпрограмма взятия значения переменной
-  {
-    ifstream iniFile(this->pathToFile); // открываем ini-файл
-    if (iniFile.is_open() == false)     // если не открылся...
+    if (iniFile.is_open() == false) // если не открылся...
     {
-      // cout << "File \"" << this->pathToFile << "\" unknow." << endl; //...сообщаем об этом
-      iniFile.close(); // закрываем файл,
-      throw invalid_argument("ERROR! File \"" + this->pathToFile + "\" ne najden.");
-      return "error"; // прерываем подпрограмму
+      iniFile.close();                                                           // закрываем файл
+      throw invalid_argument("Oshibka! \"" + pathAndNameFile + "\" ne najden!"); // выводим ошибку (сообщаем)
     };
 
-    string sectionName = "";  // название секции
-    string variableName = ""; // название переменной
-
-    size_t positionPointSeparator = adressLine.find('.');         // номер точки в строке
-    sectionName = adressLine.substr(0, positionPointSeparator);   // имя секции - первая часть
-    variableName = adressLine.substr(positionPointSeparator + 1); // имя переменной - вторая часть
-
-    string lineFromFile = "";                                      // текущая строка из файла
-    regex patternSectionName("\\[[a-zA-Z0-9_]+\\]");               // шаблон имени секции
-    regex patternTypeLine("[\\s\\S]+=[\\s\\S]*");                  // шаблон строки, содержащей знак "="
-    vector<vector<string>> treeSets = {};                          // дерево наборов
-    regex patternOurVariable(variableName + "\\s*=\\s*[\\s\\S]*"); // шаблон строки с искомой переменной
-    regex patternErrorLine("[\\s\\S]+=\\s*");                      // шаблон ошибки строки
-    int lineCount = 1;                                             // подсчёт строк
+    string lineFromFile = "";                                          // строка из файла
+    regex patternSectionName("\\s*\\[Section[0-9]+\\]\\s*");           // имя секции
+    regex patternInfoLine("\\s*[a-zA-Z]\\w*\\s*=[^;]*[\\w][\\s\\S]*"); // инф. строка
+    regex patternCommentLine("\\s*;[\\s\\S]*");                        // строка коммент.
+    regex patternEmptyLine("\\s*");                                    // пустая строка
+    string currentSectionName = "";                                    // текущ. секц.
+    int countLine = 1;                                                 // подсчёт строк
 
     while (true) // парсим файл
     {
@@ -48,62 +36,129 @@ public:
         iniFile.close(); //...закрываем его
         break;           //...прерываем цикл
       };
-      lineFromFile.clear();                                  // очищаем текущую строку
-      getline(iniFile, lineFromFile);                        // читаем строку из файла
-      this->cleansUpSpacesInBeginningAndInEnd(lineFromFile); // пробелы в начале и конце
 
-      if (regex_match(lineFromFile, patternErrorLine))
+      lineFromFile.clear();           // очищаем строку
+      getline(iniFile, lineFromFile); // читаем строку из файла
+
+      bool itIsSection = regex_match(lineFromFile, patternSectionName);
+      bool itIsInfo = regex_match(lineFromFile, patternInfoLine);
+      bool itIsComment = regex_match(lineFromFile, patternCommentLine);
+      bool itIsEmpty = regex_match(lineFromFile, patternEmptyLine);
+
+      if (itIsSection)
       {
-        throw invalid_argument("ERROR! At line " + to_string(lineCount) + ": " + lineFromFile);
+        currentSectionName.clear();
+        currentSectionName = lineFromFile;
+        this->cleansUpSpacesInBeginningAndInEnd(currentSectionName);
+        currentSectionName.pop_back();
+        currentSectionName.erase(0, 1);
+        this->treeMembers[currentSectionName][""] = "";
+        countLine++;
+        continue;
       };
 
-      if (regex_match(lineFromFile, patternSectionName)) // если имя секции
+      if (itIsInfo)
       {
-        treeSets.push_back({lineFromFile});
+        string variableName = lineFromFile.substr(0, lineFromFile.find('='));
+        this->cleansUpSpacesInBeginningAndInEnd(variableName);
+        string valueAndComment = lineFromFile.substr(lineFromFile.find('=') + 1);
+        string variableValue = "";
+        if (valueAndComment.find(';') != string::npos)
+        {
+          variableValue = valueAndComment.substr(0, valueAndComment.find(';'));
+        }
+        else
+        {
+          variableValue = valueAndComment;
+        };
+        this->cleansUpSpacesInBeginningAndInEnd(variableValue);
+        this->treeMembers[currentSectionName][variableName] = variableValue;
+        countLine++;
+        continue;
       };
 
-      if (regex_match(lineFromFile, patternTypeLine)) // если тип строки
+      if (!itIsSection && !itIsInfo && !itIsComment && !itIsEmpty)
       {
-        treeSets[treeSets.size() - 1].push_back(lineFromFile);
+        throw invalid_argument("ERROR at line " + to_string(countLine) + ": \"" + lineFromFile + "\"");
+        countLine++; // глупость
+        continue;    // ещё большая
       };
-      lineCount++;
+      countLine++;
+    };
+  };
+
+  template <typename T>
+  T getValue(string section, string variable) // подпрограмма взятия значения переменной
+  {
+    T universalResult;
+
+    if (this->treeMembers.find(section) == this->treeMembers.end())
+    {
+      cout << "Net sekcii \"" << section << "\", a est:" << endl;
+
+      for (auto const &[sect, map] : this->treeMembers)
+      {
+        cout << sect << endl;
+      };
+
+      throw invalid_argument("Ne najdena sekciya \"" + section + "\"");
+      return universalResult;
     };
 
-    string endResult = ""; // строка окончательного результата
-
-    for (int Set = 0; Set < treeSets.size(); Set++)
+    if (this->treeMembers[section].find(variable) == this->treeMembers[section].end())
     {
-      if (treeSets[Set][0] == sectionName)
+      cout << "V sekcii \"" << section << "\" net peremennoj \"" << variable << "\", a est:" << endl;
+
+      for (auto const &[variable, value] : this->treeMembers[section])
       {
-        for (int Line = 1; Line < treeSets[Set].size(); Line++)
-        {
-          if (regex_match(treeSets[Set][Line], patternOurVariable))
-          {
-            size_t positionParitySignum = treeSets[Set][Line].find('=');            // место знака "равно"
-            string mezoLine = treeSets[Set][Line].substr(positionParitySignum + 1); // строка после знака "равно"
+        cout << variable << endl;
+      };
 
-            string finishedLine = "";
-            size_t positionCommentSignum = mezoLine.find(';'); // место комментария                                                                // финишн. строка
-            if (positionCommentSignum != string::npos)
-            {
-              finishedLine = mezoLine.substr(0, positionCommentSignum);
-            }
-            else
-            {
-              finishedLine = mezoLine;
-            };
-            cleansUpSpacesInBeginningAndInEnd(finishedLine);
-            endResult.clear();
-            endResult = finishedLine;
-          }
-        }
-      }
-    }
-    if (endResult == "")
+      throw invalid_argument("Net peremennoj \"" + variable + "\"");
+      return universalResult;
+    };
+
+    string protoResult = this->treeMembers[section][variable];
+
+    regex patternIntegerNumber("-?[0-9]+");
+    regex patternDoubleNumber("-?[0-9]+\\.[0-9]+");
+    regex patternStringLine("[\\s\\S]*[a-zA-Z][\\s\\S]*");
+
+    if constexpr (is_same_v<T, int>)
     {
-      throw invalid_argument("ERROR! Peremennaya \"" + variableName + "\" ne najdena v \"" + sectionName + "\"");
-    }
-    return endResult; // прерываем подпрограмму
+      if (regex_match(protoResult, patternIntegerNumber))
+      {
+        universalResult = stoi(protoResult);
+        return universalResult;
+      };
+      throw invalid_argument("Oshibka identifikacii tipa!");
+      return universalResult;
+    };
+
+    if constexpr (is_same_v<T, double>)
+    {
+      if (regex_match(protoResult, patternDoubleNumber))
+      {
+        universalResult = stod(protoResult);
+        return universalResult;
+      };
+      throw invalid_argument("Oshibka identifikacii tipa!");
+      return universalResult;
+    };
+
+    if constexpr (is_same_v<T, std::string>)
+    {
+      if (regex_match(protoResult, patternStringLine))
+      {
+        universalResult = protoResult;
+        return universalResult;
+      };
+      throw invalid_argument("Oshibka identifikacii tipa!");
+      return universalResult;
+    };
+
+    throw invalid_argument("Zadan nepredusmotrennyij tip!");
+    return universalResult;
   };
 
   void cleansUpSpacesInBeginningAndInEnd(string &line)
@@ -114,47 +169,93 @@ public:
   }
 
 private:
-  string pathToFile = "";
+  map<string, map<string, string>> treeMembers; // дерево структуры;
 };
 
 int main()
 {
-  cout << endl;
   ini_parser parser("./config.ini");
-
   try
   {
-    cout << parser.getValue("[Section1].var1") << endl;
-    cout << parser.getValue("[Section1].var2") << endl;
-    cout << parser.getValue("[Section1].var10") << endl;
+    cout << parser.getValue<double>("Section1", "var1") << endl;
+    cout << parser.getValue<string>("Section1", "var2") << endl;
+    cout << parser.getValue<string>("Section1", "var3") << endl;
+    cout << parser.getValue<int>("Section2", "var1") << endl;
+    cout << parser.getValue<string>("Section2", "var2") << endl;
+    cout << parser.getValue<int>("Section2", "var3") << endl;
+    cout << parser.getValue<string>("Section4", "Mode") << endl;
+    cout << parser.getValue<string>("Section4", "Vid") << endl;
   }
-  catch (const invalid_argument &e)
+  catch (const std::exception &e)
   {
-    cout << e.what() << endl; // ошибка ненайденной переменной
+    std::cerr << e.what() << '\n';
   }
 
   cout << endl;
-  ini_parser parser2("./configg.ini");
 
   try
   {
-    cout << parser2.getValue("[Section1].var1") << endl;
+    cout << parser.getValue<double>("Section10", "var10") << endl;
   }
-  catch (const invalid_argument &e)
+  catch (const std::exception &e)
   {
-    cout << e.what() << endl; // ошибка непрочитанного файла
+    std::cerr << e.what() << '\n';
   }
 
   cout << endl;
-  ini_parser parser3("./configErr.ini");
 
   try
   {
-    cout << parser3.getValue("[Section2].var2") << endl;
+    cout << parser.getValue<double>("Section1", "var10") << endl;
   }
-  catch (const invalid_argument &e)
+  catch (const std::exception &e)
   {
-    cout << e.what() << endl; //
+    std::cerr << e.what() << '\n';
   }
+
+  cout << endl;
+
+  try
+  {
+    cout << parser.getValue<string>("Section1", "var1") << endl;
+  }
+  catch (const std::exception &e)
+  {
+    std::cerr << e.what() << '\n';
+  }
+
+  cout << endl;
+
+  try
+  {
+    cout << parser.getValue<bool>("Section1", "var1") << endl;
+  }
+  catch (const std::exception &e)
+  {
+    std::cerr << e.what() << '\n';
+  }
+
+  cout << endl;
+
+  try
+  {
+    ini_parser parser2("./configg.ini");
+  }
+  catch (const std::exception &e)
+  {
+    std::cerr << e.what() << '\n';
+  }
+
+  cout << endl;
+
+  try
+  {
+    ini_parser parser3("./config2.ini");
+  }
+  catch (const std::exception &e)
+  {
+    std::cerr << e.what() << '\n';
+  }
+
   return 0;
-}
+};
